eb0158ea5bc57d542bca0a4b0eb13610949de6cd
alien1900 <alien1900@bk.ru>
2018-02-26 15:25:46 +0700

    Final

--- app/src/main/java/com/example/ivanovnv/firsttaskforevalution/SecondActivity.java
+++ app/src/main/java/com/example/ivanovnv/firsttaskforevalution/SecondActivity.java
@@ -1,19 +1,33 @@
 package com.example.ivanovnv.firsttaskforevalution;
 
+import android.app.SearchManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-/**
- * Created by IvanovNV on 21.02.2018.
- */
 
 public class SecondActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private Button mButton;
     public static String ID_TEXT = "ID_TEXT";
+
+    View.OnClickListener mOnButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_WEB_SEARCH);
+            intent.putExtra(SearchManager.QUERY,mTextView.getText().toString());
+
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,5 +39,8 @@ public class SecondActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         mTextView.setText(bundle.getString(ID_TEXT));
+
+        mButton = findViewById(R.id.bt_openlink);
+        mButton.setOnClickListener(mOnButtonClickListener);
     }
 }
